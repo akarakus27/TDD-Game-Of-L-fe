684c648b66c2b95525a601c4e1b1d278d656ca32
Abdulkadir Karakuş <15541028@firat.edu.tr>
2019-12-26 02:00:59 +0300

    Add files via upload

---
+++ Conways.Game.Of.Life/GameOfLİfe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conways.Game.Of.Life
+{
+    class GameOfLİfe
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

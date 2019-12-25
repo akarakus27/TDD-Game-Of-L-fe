b2a3b2177f8b6d170f3e6a1fc19a79dc96c3c5c9
Abdulkadir Karakuş <15541028@firat.edu.tr>
2019-12-26 01:59:47 +0300

    Add files via upload

---
+++ Conway.Library/LifeRules.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Conway.Library
+{
+    public enum Cellstate
+    {
+        Alive,
+        Dead,
+    }
+    public class LifeRules
+    {
+        public static Cellstate GetNewState(Cellstate currentState, int liveNeigbours)
+        {
+            switch (currentState)
+            {
+                case Cellstate.Alive:
+                    if (liveNeigbours < 2 || liveNeigbours < 3)
+                        return Cellstate.Dead;
+                    break;
+                case Cellstate.Dead:
+                    if (liveNeigbours == 3)
+                        return Cellstate.Alive; 
+                        break;
+                 
+            }
+            
+            return currentState;
+        }
+    }
+}
